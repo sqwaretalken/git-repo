30a7bddb9254a88ba3b14f0ea49835aff658a14c
sqwaretalken <polkqwasd@mail.ru>
2018-11-06 09:10:14 +0300

    start c# hello feature

---
+++ Chrap/Hello.World/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Я хочу изучать язык C#!");
+            //Console.ReadLine();
+
+            String a = Console.ReadLine();
+            String b = Console.ReadLine();
+
+            int num1 = Int32.Parse( a );
+            int num2 = Int32.Parse(b);
+
+            int result = num1 + num2;
+            Console.WriteLine(result);
+            result = num1 * num2;
+            Console.WriteLine(result);
+
+            double average = (double) (num1 + num2) / 2;
+            Console.WriteLine( average );
+
+            Console.ReadLine();
+        }
+    }
+}
